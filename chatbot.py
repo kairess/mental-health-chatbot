05c70b67b74a94a4656a1c7bdefb2217cb853938
younga <younga.hci.du@gmail.com>
2023-05-28 08:30:10 +0900

    화상 동상 데이터

--- chatbot.py
+++ chatbot.py
@@ -11,13 +11,14 @@ def cached_model():
     return model
 
 @st.cache_data()
-def get_dataset():
-    df = pd.read_csv('C:\\Users\\21813903\\Desktop\\chatbot2\\mental-health-chatbot\\wellness_dataset.csv')
+def get_dataset(file_path):
+    df = pd.read_csv(file_path)
     df['embedding'] = df['embedding'].apply(json.loads)
     return df
 
 model = cached_model()
-df = get_dataset()
+wellness_df = get_dataset('C:\\Users\\21813903\\Desktop\\chatbot2\\mental-health-chatbot\\wellness_dataset.csv')
+symptom_df = get_dataset('C:\\Users\\21813903\\Desktop\\chatbot2\\mental-health-chatbot\\sysmptom_data.csv')
 
 st.header('심리상담 챗봇')
 st.markdown("[❤️빵형의 개발도상국](https://www.youtube.com/c/빵형의개발도상국)")
@@ -30,13 +31,22 @@ if 'past' not in st.session_state:
 
 with st.form('form', clear_on_submit=True):
     user_input = st.text_input('당신: ', '')
+    image_input = st.file_uploader('이미지 업로드:', type=['jpg'], accept_multiple_files=False)
     submitted = st.form_submit_button('전송')
 
 if submitted and user_input:
     embedding = model.encode(user_input)
 
-    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
-    answer = df.loc[df['distance'].idxmax()]
+    wellness_df['distance'] = wellness_df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    symptom_df['distance'] = symptom_df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+
+    wellness_answer = wellness_df.loc[wellness_df['distance'].idxmax()]
+    symptom_answer = symptom_df.loc[symptom_df['distance'].idxmax()]
+
+    if wellness_answer['distance'] > symptom_answer['distance']:
+        answer = wellness_answer
+    else:
+        answer = symptom_answer
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(answer['챗봇'])
